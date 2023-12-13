1cc659d80a21218311b0ffb2e6d4dd78f3eadedd
Keo  Kay <keokay888@gmail.com>
2023-12-13 10:22:45 +0700

    updates configuration

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Working with Kafka simple way 
 ### Requirement 
-1. Having containers that run the zookeeper and broker of the kafka
+1. Having containers that run the **zookeeper** and **broker** of the kafka
---
+++ src/main/java/lol/anuznomii/springkafka/MessageRequest.java
@@ -0,0 +1,12 @@
+package lol.anuznomii.springkafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequest {
+    private String message;
+}
